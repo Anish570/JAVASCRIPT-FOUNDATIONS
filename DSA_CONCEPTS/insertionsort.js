fbc9209a1d1f26fd7da561068d61c893c3ece09e
anish <anishgautam2024@gmail.com>
2024-08-10 18:08:46 +0545

    insertion sort practised

--- DSA_CONCEPTS/insertionsort.js
+++ DSA_CONCEPTS/insertionsort.js
@@ -1,1 +1,15 @@
-let arr = [3, 2, 5, 9, 7];
+let arr = [3, 2, 5, 9, 7, 1];
+
+function sort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let numbertoinset = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > numbertoinset) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = numbertoinset;
+  }
+  return arr;
+}
+console.log(sort(arr));
