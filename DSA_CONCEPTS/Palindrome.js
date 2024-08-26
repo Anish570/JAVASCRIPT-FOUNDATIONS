91ff78f929c2fa401135d541cc71d8d947f0f5d8
anish <anishgautam2024@gmail.com>
2024-08-26 21:35:54 +0545

    practised palindrome after learning the concept of reversing

--- DSA_CONCEPTS/Palindrome.js
+++ DSA_CONCEPTS/Palindrome.js
@@ -1,6 +1,23 @@
-const isPalindrome = (x) => {
-  x < 0 && false;
-  const reverse = x.toString().split("").reverse().join("");
-  return x.toString() === reverse;
+function reverseStr(str) {
+  reversed = "";
+  for (char of str) {
+    reversed = char + reversed;
+  }
+  return reversed;
+}
+const isPalindrome = (val) => {
+  if (typeof val === "string") {
+    const reverse = reverseStr(val);
+    return reverse === val;
+  }
+  if (typeof val === "number") {
+    val < 0 && false;
+    const reverse = val.toString().split("").reverse().join("");
+    return val.toString() === reverse;
+  }
+  return false;
 };
 
+console.log(isPalindrome("lol"));
+console.log(isPalindrome(113311));
+console.log(isPalindrome(-113311));
